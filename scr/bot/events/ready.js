fc910d9f24b1a9dc0f92d9021e17e02d85d23a55
Sobhan.SRZA <90289153+Sobhan-SRZA@users.noreply.github.com>
2022-01-07 16:16:46 +0330

    Delete scr directory

--- scr/bot/events/ready.js
+++
@@ -1,7 +0,0 @@
-const chalk = require('chalk');
-const figlet = require("figlet");
-module.exports = async function(client) {
-    console.log(chalk.yellow.bold(figlet.textSync("AMG OP!")));
-    await console.log(chalk.red.bold(client.user.tag) + chalk.blue.bold("Is Ready"));
-
-}
